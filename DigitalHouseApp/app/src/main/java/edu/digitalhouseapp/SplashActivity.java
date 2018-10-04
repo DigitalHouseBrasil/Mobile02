d7abd7f7e154265597f46bfe3a8875dfab4f4af4
Ernanni <ernanni42@gmail.com>
2018-10-04 19:09:44 -0300

    xml de login completo

---
+++ DigitalHouseApp/app/src/main/java/edu/digitalhouseapp/SplashActivity.java
@@ -0,0 +1,29 @@
+package edu.digitalhouseapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+
+		new Timer().schedule(new TimerTask() {
+			@Override
+			public void run() {
+				jump(null);
+			}
+		}, 1500);
+	}
+
+	public void jump(View view) {
+		startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+	}
+}
