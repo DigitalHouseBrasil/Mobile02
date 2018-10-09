eb437944fe28023e1ed9549a9f8630901838a410
LeonardoMarquesSilva <leonardomarques84@hotmail.com>
2018-10-08 22:30:33 -0300

    Atualizado

--- DigitalHouseApp/app/src/main/java/br/com/digitalhouse/digitalhouseapp/HomeActivity.java
+++ DigitalHouseApp/app/src/main/java/br/com/digitalhouse/digitalhouseapp/HomeActivity.java
@@ -1,9 +1,15 @@
 package br.com.digitalhouse.digitalhouseapp;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import br.com.digitalhouse.digitalhouseapp.fragments.CommentsFragment;
+import br.com.digitalhouse.digitalhouseapp.fragments.PostsFragment;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -12,12 +18,32 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        TextView email = findViewById(R.id.email_home);
 
-        Intent intent = getIntent();
+    }
 
-        Bundle bundle = intent.getExtras();
+    public void changeFragmentOne(View view) {
 
-        email.setText(bundle.getString("email"));
+//Instancia o fragment de Posts
+        Fragment fragment = new PostsFragment();
+        // chama o replace passando uma instancia de fragmento de posts
+        replaceFragment(fragment, R.id.content_one, "POSTS");
+    }
+
+    public void changeFragmentTwo(View view) {
+//// chama o replace passando uma instancia de fragmento de comentarios
+        replaceFragment(new CommentsFragment(), R.id.content_two, "COMMENTS");
+
+    }
+
+    // substitui o container com o fragment passado e adiciona a pilha
+    public void replaceFragment(Fragment fragment, int container, String stack) {
+// inicia a transação
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//substitui container com fragmento
+        transaction.replace(container, fragment);
+//adiciona a pilha de fragmentos
+        transaction.addToBackStack(stack);
+//comita/finaliza a transação
+        transaction.commit();
     }
 }
