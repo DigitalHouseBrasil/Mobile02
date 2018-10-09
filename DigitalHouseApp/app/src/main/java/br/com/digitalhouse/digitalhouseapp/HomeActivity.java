e47638521010ed5188e72121f685d18ae6b3588b
William Ribeiro <wlmrs.sp@gmail.com>
2018-10-08 22:28:46 -0300

    comentario de William

--- DigitalHouseApp/app/src/main/java/br/com/digitalhouse/digitalhouseapp/HomeActivity.java
+++ DigitalHouseApp/app/src/main/java/br/com/digitalhouse/digitalhouseapp/HomeActivity.java
@@ -1,9 +1,15 @@
 package br.com.digitalhouse.digitalhouseapp;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import br.com.digitalhouse.digitalhouseapp.fragments.CommentsFragment;
+import br.com.digitalhouse.digitalhouseapp.fragments.PostsFragment;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -12,12 +18,35 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        TextView email = findViewById(R.id.email_home);
 
-        Intent intent = getIntent();
+    }
 
-        Bundle bundle = intent.getExtras();
+    public void changeFragmentOne(View view) {
 
-        email.setText(bundle.getString("email"));
+        Fragment fragment = new PostsFragment();
+        replaceFragment(fragment, R.id.content_one, "POSTS");
+    }
+
+    public void changeFragmentTwo(View view) {
+
+        //chama o replace passando um instancia do fragmento de comentários
+        replaceFragment(new CommentsFragment(), R.id.content_two, "COMMENTS");
+    }
+
+    //substitui o container com o fragment passado e adiciona a pilha
+    public void replaceFragment(Fragment fragment, int container, String stack) {
+
+        //inicia uma transação
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        //subistitui o container com o fragmento
+        transaction.replace(container, fragment);
+
+        //adiciona a pilha de fragmentos
+        transaction.addToBackStack(stack);
+
+        //comita/finaliza a transação
+        transaction.commit();
+
     }
 }
---
+++ DigitalHouseApp/app/src/main/java/br/com/digitalhouse/digitalhouseapp/fragments/PostsFragment.java
@@ -0,0 +1,27 @@
+package br.com.digitalhouse.digitalhouseapp.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.com.digitalhouse.digitalhouseapp.R;
+
+public class PostsFragment extends Fragment{
+
+    public PostsFragment(){
+        //Construtor padrão
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_posts, container, false);
+
+        return view;
+    }
+}
