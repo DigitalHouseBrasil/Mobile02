0826c427947ac3872496915121b6dc2b970291ec
William Ribeiro <wlmrs.sp@gmail.com>
2018-10-10 20:24:47 -0300

    add classe de post

---
+++ DigitalHouseApp/app/src/main/java/br/com/digitalhouse/digitalhouseapp/fragments/FragmentClick.java
@@ -0,0 +1,9 @@
+package br.com.digitalhouse.digitalhouseapp.fragments;
+
+import br.com.digitalhouse.digitalhouseapp.model.Post;
+
+public interface FragmentClick {
+    void onItemClick(Post post);
+
+
+}
