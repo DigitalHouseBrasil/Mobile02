2b12f83cef7b5a050dcb24f402c13586abd65f88
fabiotadashi <miyasato.fabio@gmail.com>
2018-10-11 22:34:55 -0300

    recycler view with interface

--- DigitalHouseApp/app/src/main/java/br/com/digitalhouse/digitalhouseapp/adapter/RecyclerViewPostAdapter.java
+++ DigitalHouseApp/app/src/main/java/br/com/digitalhouse/digitalhouseapp/adapter/RecyclerViewPostAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -12,18 +13,29 @@ import java.util.List;
 import br.com.digitalhouse.digitalhouseapp.R;
 import br.com.digitalhouse.digitalhouseapp.model.Post;
 
-public class RecyclerViewPostAdapter extends RecyclerView.Adapter<RecyclerViewPostAdapter.ViewHolder> {
+public class RecyclerViewPostAdapter extends RecyclerView.Adapter<RecyclerViewPostAdapter
+        .ViewHolder> {
+
+    public interface OnCardClickListener {
+
+        void onShareClick(Post post);
+
+    }
 
     private List<Post> postList;
 
-    public RecyclerViewPostAdapter(List<Post> postList){
+    private OnCardClickListener listener;
+
+    public RecyclerViewPostAdapter(List<Post> postList, OnCardClickListener listener) {
         this.postList = postList;
+        this.listener = listener;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.content_post_item, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.content_post_item,
+                parent, false);
         return new ViewHolder(view);
     }
 
@@ -42,18 +54,27 @@ public class RecyclerViewPostAdapter extends RecyclerView.Adapter<RecyclerViewPo
 
         private TextView titulo;
         private TextView descricao;
+        private ImageView share;
 
         public ViewHolder(View itemView) {
             super(itemView);
 
             titulo = itemView.findViewById(R.id.post_item_title_id);
             descricao = itemView.findViewById(R.id.post_item_description_id);
+            share = itemView.findViewById(R.id.image_share_post_id);
         }
 
         // Gerencia conteudo da celula baseado num objeto Post
-        public void bind(Post post){
+        public void bind(final Post post) {
             titulo.setText(post.getTitle());
             descricao.setText(post.getDescription());
+
+            share.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listener.onShareClick(post);
+                }
+            });
         }
     }
 }
