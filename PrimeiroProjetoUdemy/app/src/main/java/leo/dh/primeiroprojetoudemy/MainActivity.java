4f15ae23553e9de16a019a4471c1496c3ac8eb3b
LeonardoMarquesSilva <leonardomarques84@hotmail.com>
2018-10-17 19:25:53 -0300

    15/10/2018

--- PrimeiroProjetoUdemy/app/src/main/java/leo/dh/primeiroprojetoudemy/MainActivity.java
+++ PrimeiroProjetoUdemy/app/src/main/java/leo/dh/primeiroprojetoudemy/MainActivity.java
@@ -2,12 +2,22 @@
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+  public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void alterarTexto (View view){
+
+        TextView texto = (TextView) findViewById(R.id.textoExibicao);
+        texto.setText("Texto alterado");
+
+    }
+
 }
--- SorteioUdemy/app/src/main/java/leo/dh/sorteioudemy/MainActivity.java
+++ SorteioUdemy/app/src/main/java/leo/dh/sorteioudemy/MainActivity.java
@@ -2,6 +2,10 @@ package leo.dh.sorteioudemy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void sortearNumero (View view){
+
+        TextView textoResultado = findViewById(R.id.textoResultado);
+
+        int x = new Random().nextInt(10);
+
+        textoResultado.setText("O numero sorteado è: " + x );
+
+    }
+
 }
