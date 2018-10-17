e171b7ab3319a721950a872f5be8a62c5a45760c
giovannitf <giovanni.t.finotello@gmail.com>
2018-10-17 19:24:44 -0300

    Aula NavigationView

--- DigitalHouseApp/app/src/main/java/br/com/digitalhouse/digitalhouseapp/HomeActivity.java
+++ DigitalHouseApp/app/src/main/java/br/com/digitalhouse/digitalhouseapp/HomeActivity.java
@@ -1,10 +1,9 @@
 package br.com.digitalhouse.digitalhouseapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,8 +12,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import br.com.digitalhouse.digitalhouseapp.fragments.CommentsFragment;
+import br.com.digitalhouse.digitalhouseapp.interfaces.FragmentClick;
+import br.com.digitalhouse.digitalhouseapp.model.Post;
+
 public class HomeActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, FragmentClick{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +26,7 @@ public class HomeActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -80,17 +76,13 @@ public class HomeActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_posts) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_comments) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_events) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_colearning) {
 
         }
 
@@ -98,4 +90,33 @@ public class HomeActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    // Substitui o conteiner com o fragment passado e adiciona a pilha
+    public void replaceFragment(Fragment fragment, int container, String stack) {
+        //Inicia uma transação
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        // Substitu o container com o fragmento
+        transaction.replace(container, fragment);
+
+        // Adiciona a pilha de fragmentos
+        transaction.addToBackStack(stack);
+
+        // Comita/Finaliza a transação
+        transaction.commit();
+    }
+
+    @Override
+    public void onItemClick(Post post) {
+        // Chama o replace passando uma instancia do fragmento de comentários
+        Fragment fragment = new CommentsFragment();
+
+        Bundle bundle = new Bundle();
+        bundle.putString("TEXT", post.getDescription());
+
+        fragment.setArguments(bundle);
+
+        replaceFragment(fragment, R.id.content_one, "COMMENTS");
+    }
+
 }
--- DigitalHouseApp/app/src/main/java/br/com/digitalhouse/digitalhouseapp/HomeActivity2.java
+++ DigitalHouseApp/app/src/main/java/br/com/digitalhouse/digitalhouseapp/HomeActivity2.java
@@ -10,12 +10,12 @@ import br.com.digitalhouse.digitalhouseapp.fragments.PostsFragment;
 import br.com.digitalhouse.digitalhouseapp.interfaces.FragmentClick;
 import br.com.digitalhouse.digitalhouseapp.model.Post;
 
-public class HomeActivity extends AppCompatActivity implements FragmentClick {
+public class HomeActivity2 extends AppCompatActivity implements FragmentClick {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_home_2);
 
         // Chama o replace passando uma instancia do fragmento de posts
         replaceFragment(new PostsFragment(), R.id.content_one, "POSTS");
