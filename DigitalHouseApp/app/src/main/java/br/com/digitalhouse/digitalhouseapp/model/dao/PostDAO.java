1fa255f29e63501a8b337805da7f96e9f152cfea
LeonardoMarquesSilva <leonardomarques84@hotmail.com>
2018-10-18 22:42:53 -0300

    18/10

---
+++ DigitalHouseApp/app/src/main/java/br/com/digitalhouse/digitalhouseapp/model/dao/PostDAO.java
@@ -0,0 +1,44 @@
+package br.com.digitalhouse.digitalhouseapp.model.dao;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import br.com.digitalhouse.digitalhouseapp.model.Post;
+
+public class PostDAO {
+
+    public List<Post> getPosts(Context context) {
+
+        List<Post> postList = new ArrayList<>();
+        try {
+
+            Gson gson = new Gson();
+
+            //Criar um stream para ler o arquivo JSON.
+            AssetManager manager = context.getAssets();
+            InputStream newsJson = manager.open("posts.json");
+            BufferedReader bufferReaderIn = new BufferedReader(new InputStreamReader(newsJson));
+
+//Utilizando o objeto gson e o método fromJson, fazer a análise do arquivo que temos no bufferReaderIn, usando como “molde” a classe News.
+
+            Post[] postArray = gson.fromJson(bufferReaderIn, Post[].class);
+
+            postList = Arrays.asList(postArray);
+
+        } catch (IOException exception) {
+            Log.e("JSON", "erro ao ler arquivo posts.json");
+        }
+        return postList;
+    }
+}
