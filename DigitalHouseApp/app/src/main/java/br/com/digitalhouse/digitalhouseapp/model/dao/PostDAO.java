38376df1a8e9dc5e9a70cd0b2c6fba8b4080ebe7
tairoroberto <tairoroberto@hotmail.com>
2018-10-22 22:36:56 -0300

    add retrofit + RX + java 1.8

--- DigitalHouseApp/app/src/main/java/br/com/digitalhouse/digitalhouseapp/model/dao/PostDAO.java
+++ DigitalHouseApp/app/src/main/java/br/com/digitalhouse/digitalhouseapp/model/dao/PostDAO.java
@@ -2,6 +2,8 @@ package br.com.digitalhouse.digitalhouseapp.model.dao;
 
 import android.content.Context;
 import android.content.res.AssetManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.util.Log;
 
 import com.google.gson.Gson;
@@ -14,13 +16,60 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import br.com.digitalhouse.digitalhouseapp.interfaces.ServiceListener;
 import br.com.digitalhouse.digitalhouseapp.model.Post;
+import br.com.digitalhouse.digitalhouseapp.model.dao.network.RetrofitService;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class PostDAO {
 
-    public List<Post> getPosts(Context context) {
+    public void getPosts(Context context, final ServiceListener listener) {
         List<Post> postList = new ArrayList<>();
 
+        if (isConnected(context)) {
+            getNetworkDataRX(listener);
+
+        } else {
+            getLocalData(context, listener);
+        }
+    }
+
+    private void getNetworkData(final ServiceListener listener) {
+        Call<List<Post>> call = RetrofitService.getApiService().getPosts();
+
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if (response.body() != null) {
+                    listener.onSuccess(response.body());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                listener.onError(t);
+            }
+        });
+    }
+
+    private void getNetworkDataRX(final ServiceListener listener) {
+
+        RetrofitService.getApiService().getPostsRX()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(posts -> {
+                    listener.onSuccess(posts);
+                }, throwable -> {
+                    listener.onError(throwable);
+                });
+    }
+
+    private void getLocalData(Context context, ServiceListener listener) {
+        List<Post> postList;
         try {
             //Criar um stream para ler o arquivo JSON.
             AssetManager manager = context.getAssets();
@@ -37,11 +86,24 @@ public class PostDAO {
             // Transforma Array[] em ArrayList
             postList = Arrays.asList(postArray);
 
-        }catch (IOException exception){
-            Log.e("JSON", "erro ao ler arquivo posts.json");
-        }
+            listener.onSuccess(postList);
 
-        return postList;
+        } catch (IOException exception) {
+            Log.e("JSON", "erro ao ler arquivo posts.json");
+            listener.onError(exception);
+        }
     }
 
+    private boolean isConnected(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo;
+
+        if (connectivityManager != null) {
+            networkInfo = connectivityManager.getActiveNetworkInfo();
+            return networkInfo != null && networkInfo.isConnected() &&
+                    (networkInfo.getType() == ConnectivityManager.TYPE_WIFI
+                            || networkInfo.getType() == ConnectivityManager.TYPE_MOBILE);
+        }
+        return false;
+    }
 }
