a5f161802c8fb9499129fbaa563cd46a8b30e6d0
tairoroberto <tairoroberto@hotmail.com>
2018-10-22 22:41:16 -0300

    Commets

--- DigitalHouseApp/app/src/main/java/br/com/digitalhouse/digitalhouseapp/model/dao/PostDAO.java
+++ DigitalHouseApp/app/src/main/java/br/com/digitalhouse/digitalhouseapp/model/dao/PostDAO.java
@@ -12,7 +12,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -28,22 +27,27 @@ import retrofit2.Response;
 public class PostDAO {
 
     public void getPosts(Context context, final ServiceListener listener) {
-        List<Post> postList = new ArrayList<>();
 
+        /* Se estamos conectados a internet chamamos a api*/
         if (isConnected(context)) {
             getNetworkDataRX(listener);
 
         } else {
+            /* se não temos conexão pegamos os posts da pasta assets*/
             getLocalData(context, listener);
         }
     }
 
     private void getNetworkData(final ServiceListener listener) {
+
+        //Prepara a chamada para a  API
         Call<List<Post>> call = RetrofitService.getApiService().getPosts();
 
+        // Coloca a chamada na fila
         call.enqueue(new Callback<List<Post>>() {
             @Override
             public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                // Se a resposta não for nula disparamos o listener de sucesso
                 if (response.body() != null) {
                     listener.onSuccess(response.body());
                 }
@@ -51,19 +55,24 @@ public class PostDAO {
 
             @Override
             public void onFailure(Call<List<Post>> call, Throwable t) {
+                // disparamos o listener de sucesso
                 listener.onError(t);
             }
         });
     }
 
+
+    /* Chamada do api de posts usando RxJava*/
     private void getNetworkDataRX(final ServiceListener listener) {
 
         RetrofitService.getApiService().getPostsRX()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io()) //faz a requisição na thread de entreada e saida de dado
+                .observeOn(AndroidSchedulers.mainThread()) // quando retornar expoe os dados na main thread
                 .subscribe(posts -> {
+                    // dispara o listener de sucesso
                     listener.onSuccess(posts);
                 }, throwable -> {
+                    // dispara o listener de erro
                     listener.onError(throwable);
                 });
     }
