53de428392116d625ec06697991b64b4c07606b9
tairoroberto <tairoroberto@hotmail.com>
2018-10-24 18:38:10 -0300

    delete

--- RetofitPagination/app/src/main/java/br/com/digitalhouse/retofitpagination/interfaces/WebServiceInterface.java
+++
@@ -1,7 +0,0 @@
-package br.com.digitalhouse.retofitpagination.interfaces;
-
-//integrar as camadas (entrega a resposta do controller p/ view)
-public interface WebServiceInterface {
-	void success(Object obj);
-	void erro(Throwable throwable);
-}
