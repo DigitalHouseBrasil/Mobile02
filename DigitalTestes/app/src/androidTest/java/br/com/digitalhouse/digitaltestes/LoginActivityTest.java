c07cca902e93f17f06c28f611394fc78266f5444
tairoroberto <tairoroberto@hotmail.com>
2018-11-26 20:31:22 -0200

    add testes

--- DigitalTestes/app/src/androidTest/java/br/com/digitalhouse/digitaltestes/LoginActivityTest.java
+++ DigitalTestes/app/src/androidTest/java/br/com/digitalhouse/digitaltestes/LoginActivityTest.java
@@ -4,12 +4,27 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
 public class LoginActivityTest {
 
     @Rule
     public TestRule activityRule = new ActivityTestRule<>(LoginActivity.class);
+
+    @Test
+    public void testIfButtonIsShowing() {
+        onView(withId(R.id.edittext_user_login)).perform(replaceText("tairo@digitalhouse.com"));
+        onView(withId(R.id.edittext_password_login)).perform(replaceText("123456"));
+        onView(withId(R.id.btn_goto_login)).perform(scrollTo()).perform(click());
+    }
+
 }
--- DigitalTestes/app/src/main/java/br/com/digitalhouse/digitaltestes/LoginActivity.java
+++ DigitalTestes/app/src/main/java/br/com/digitalhouse/digitaltestes/LoginActivity.java
@@ -3,6 +3,7 @@ package br.com.digitalhouse.digitaltestes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,6 +14,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void login(View view) {
+        Toast.makeText(this, "Login com sucesso", Toast.LENGTH_SHORT).show();
     }
 
     public void loginWithMyFacebookButtom(View view) {
